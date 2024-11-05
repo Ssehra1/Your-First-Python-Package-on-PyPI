10fa5d9c178874545b4b7270e762ced59f5ab4a8
Ssehra1 <ssehra@conestogac.on.ca>
2024-11-05 15:54:09 -0500

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,5 @@
-# Leave it empty. This is just a special file that tells pip that your main module is in this folder. 
+from ._Class1 import Function1
+from ._Class1 import Function2
+from ._Class2 import Function1
+... # Leave it empty. This is just a special file that tells pip that your main module is in this folder. 
 # No need to add anything here. Feel free to delete this line when you make your own package.
